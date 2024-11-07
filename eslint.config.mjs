f702182e19dd87f563f787a165fd1864b8c58be5
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-11-08 03:23:44 +0900

    eslint 설정 수정 및 lint script 수정 (#3)

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'eslint-define-config';
+import reactPlugin from 'eslint-plugin-react';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+
+export default defineConfig([
+  {
+    files: ['*.ts', '*.tsx'], // TypeScript 파일에 대한 설정
+    languageOptions: {
+      parser: '@typescript-eslint/parser', // 타입스크립트 파서
+    },
+    plugins: {
+      react: reactPlugin,
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  },
+]);
