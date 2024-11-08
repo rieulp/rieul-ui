ea7ae1129f1303e74ef411c1ea41699212a5e33d
rieulp <rayoung1229@gmail.com>
2024-11-08 20:25:59 +0900

    feat: storybook 초기 설정 및 프로젝트 세팅 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 require('dotenv').config();
@@ -8,37 +7,44 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
-    clean: true, // 이전 빌드 파일 삭제
+    clean: true // 이전 빌드 파일 삭제
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src')
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'], // 처리할 파일 확장자
+    extensions: ['.ts', '.tsx', '.js', '.jsx'] // 처리할 파일 확장자
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // 타입스크립트 파일 처리 ts, tsx
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        test: /\.tsx?$/, // 타입스크립트 파일 처리 .ts, .tsx
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+            }
+          }
+        ],
+        exclude: /node_modules/ // node_modules 디렉토리 제외
+      }
+    ]
   },
   mode: process.env.NODE_ENV || 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html', // 기본 HTML 템플릿
-    }),
+      template: './public/index.html' // 기본 HTML 템플릿
+    })
   ],
   devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false, // 소스 맵
   devServer: process.env.NODE_ENV === 'development' && {
     // 개발 서버
     static: {
       // 정적 파일 서비스
-      directory: path.join(__dirname, 'dist'), // 정적 파일 경로
+      directory: path.join(__dirname, 'dist') // 정적 파일 경로
     },
     compress: true, // gzip 압축
-    port: 3000, // 포트 번호
-  },
+    port: 3000 // 포트 번호
+  }
 };
