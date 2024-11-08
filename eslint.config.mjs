db9c66797e34b34a94edb8953c72595886e5404c
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-11-08 20:29:00 +0900

    feat: storybook 초기 설정 및 프로젝트 세팅 수정 (#8)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,21 +6,21 @@ export default defineConfig([
   {
     files: ['*.ts', '*.tsx'], // TypeScript 파일에 대한 설정
     languageOptions: {
-      parser: '@typescript-eslint/parser', // 타입스크립트 파서
+      parser: '@typescript-eslint/parser' // 타입스크립트 파서
     },
     plugins: {
       react: reactPlugin,
-      '@typescript-eslint': typescriptPlugin,
+      '@typescript-eslint': typescriptPlugin
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn'
     },
     settings: {
       react: {
-        version: 'detect',
-      },
+        version: 'detect'
+      }
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier']
+  }
 ]);
