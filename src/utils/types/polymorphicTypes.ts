10c67dae2980547a0d2247ad4dd8b91453925957
rieulp <rayoung1229@gmail.com>
2024-11-22 22:31:03 +0900

    기능: 폴리모픽 컴포넌트 생성 유틸 추가
    - 다양한 ElementType을 지원하는 컴포넌트 생성 함수 구현
    - as prop을 활용한 동적 태그 지원
    - forwardRef로 ref 타입 안정성 확보

---
+++ src/utils/types/polymorphicTypes.ts
@@ -0,0 +1,29 @@
+import type {
+  ComponentPropsWithoutRef,
+  ElementType,
+  ForwardedRef,
+  PropsWithChildren,
+  PropsWithoutRef,
+  ReactElement
+} from 'react';
+import type { ElementTypeToHTMLElement } from './elementTypeUtils';
+
+/** `as` prop을 추가한 타입 */
+type withAsProp<T> = { as?: T };
+
+/** 다형성 컴포넌트의 props 타입 */
+type PolymorphicComponentProps<T extends ElementType, P = {}> = PropsWithChildren<P & withAsProp<T>> &
+  Omit<ComponentPropsWithoutRef<T>, keyof (P & withAsProp<T>)>;
+
+/** 다형성 컴포넌트 렌더링 함수 */
+export type PolymorphicRenderFunction<T extends ElementType, P = {}> = (
+  props: PropsWithoutRef<PolymorphicComponentProps<T, P>>,
+  ref: ForwardedRef<ElementTypeToHTMLElement<T>>
+) => ReactElement | null;
+
+/** 다형성 컴포넌트 타입 */
+export type PolymorphicComponent<T extends ElementType, P> = <Element extends ElementType = T>(
+  props: PolymorphicComponentProps<Element, P> & {
+    ref?: ForwardedRef<ElementTypeToHTMLElement<Element>>;
+  }
+) => ReactElement;
---
+++ src/utils/createPolymorphicComponent.ts
@@ -0,0 +1,17 @@
+import { forwardRef, ElementType } from 'react';
+import { PolymorphicComponent, PolymorphicRenderFunction } from './\btypes/polymorphicTypes';
+
+/**
+ * 다형성 컴포넌트를 생성하는 함수
+ * @param render 렌더링 함수
+ * @param displayName 컴포넌트 이름
+ * @returns 생성된 다형성 컴포넌트
+ */
+export const createPolymorphicComponent = <T extends ElementType = 'div', P = {}>(
+  render: PolymorphicRenderFunction<T, P>,
+  displayName?: string
+) => {
+  const Component = forwardRef(render);
+  Component.displayName = displayName || 'PolymorphicComponent';
+  return Component as unknown as PolymorphicComponent<T, P>;
+};
