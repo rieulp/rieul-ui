9574e6808b39751e9abaf6f51e84d6132d26e683
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-11-22 23:57:25 +0900

    설정: Tailwind CSS 설치 및 웹팩 설정 추가 (#17)
    ## 📄 작업 내용
    <!-- 이 PR에서 작업한 내용을 구체적으로 적어주세요 -->
    - Tailwind CSS 설치 및 초기화
    - 웹팩에 PostCSS 로더 추가
    - Tailwind 사용을 위한 postcss.config.js 설정
    - tailwind.config.js 파일 생성 및 경로 설정
       
    ---
    
    ## 🤔 추가 설명
    <!-- 코드 구현에 대한 추가적인 설명이나 고려 사항이 있다면 적어주세요 -->
       
    ---
    
    ## ✅ 체크리스트
    - [ ] 주요 로직에 대한 테스트 코드 작성 및 통과
    - [ ] 타입 안정성 확인
    - [ ] 기존 코드와의 통합 테스트 완료
    - [ ] 관련 문서(README 등) 업데이트
    
    ---
    
    ## 📷 스크린샷 (선택 사항)
    <!-- 동작 화면, 테스트 결과 등 시각적인 자료가 있다면 추가해주세요 -->
    
    ---
    
    ## 🔗 관련 이슈
    <!-- 관련된 이슈 번호를 적어주세요 -->
    -

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}' // 소스 코드에서 Tailwind를 적용할 파일 경로
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
